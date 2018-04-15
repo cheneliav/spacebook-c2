918fe06f309bceeef82a13883ce4d42f7bf9b0b9
cheneliav <411chen@gmail.com>
2018-04-15 20:38:25 +0300

    spacebook3

--- main.js
+++ main.js
@@ -32,7 +32,8 @@ var SpacebookApp = function () {
   var createPost = function (text) {
     var post = {
       text: text,
-      id: currentId
+      id: currentId,
+      comments : []
     }
 
     currentId += 1;
@@ -71,19 +72,50 @@ var SpacebookApp = function () {
     $clickedPost.find('.comments-container').toggleClass('show');
   }
 
+  var createComment = function (text,idc) {
+    
+    var comment = {
+      text: text
+    }
+    posts[idc].comments.push(comment);
+ 
+  }
+
+
+  var renderComments = function (idc) {
+  
+     $('.comment').empty();
+
+     for (var i = 0; i < posts[idc].comments.length; i += 1) {
+       //var comment = posts[idc].comments[i];
+
+
+      $posts.append('<div class="comment" >'
+        + '<a href="#" class="remove-com">remove</a> '  + posts[idc].comments[i].text +
+        '</div>');
+     }
+  }
+  var removeComment = function (currentCom) {
+    var $clickedCom = $(currentCom).closest('.comment');
+  
+    $clickedCom.remove();
+  }
+
+
+
   return {
     createPost: createPost,
     renderPosts: renderPosts,
     removePost: removePost,
 
     // TODO: Implement
-    // createComment: createComment,
+     createComment: createComment,
 
     // TODO: Implement
-    // renderComments: renderComments,
+     renderComments: renderComments,
 
     // TODO: Implement
-    // removeComment: removeComment,
+     removeComment: removeComment,
     toggleComments: toggleComments
   }
 }
@@ -107,4 +139,18 @@ $('.posts').on('click', '.remove', function () {
 
 $('.posts').on('click','.show-comments', function () {
   app.toggleComments(this);
+});
+
+
+$('.posts').on('click','.add-comment', function () {
+  
+  var text = $('.comment-name').val();
+  var idc = $(this).closest('.post').data().id;
+  
+  app.createComment(text,idc);
+  app.renderComments(idc);
+});
+
+$('.posts').on('click', '.remove-com', function () { 
+  app.removeComment(this);
 });
